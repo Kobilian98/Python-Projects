77c25a7ed51e4494861d532cfb02c939af8a7b3f
Noelle Reed <163303866+Kobilian98@users.noreply.github.com>
2024-04-15 15:43:30 -0400

    Update Code Academy - Receipts for Lovely Love Seats.py

--- Code Academy - Receipts for Lovely Love Seats.py
+++ Code Academy - Receipts for Lovely Love Seats.py
@@ -1,3 +1,9 @@
+'''
+A project involving assigning variables to strings and integers.
+Updating variables to reflect new totals.
+Basic arithmetic.
+'''
+
 # Item descriptions.
 lovely_loveseat_description = "Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white."
 stylish_settee_description = "Stylish Settee. Faux leather on birch. 29.50 inches high x 54.75 inches wide x 28 inches deep. Black."
